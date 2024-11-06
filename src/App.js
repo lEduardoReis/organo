df06d612a5a4f3f5ae8f6804aaf63abe5beae102
lEduardoReis <beefarias15@gmail.com>
2024-11-06 13:58:56 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner.components';
+
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem-Vindo ao React!!
         </p>
         <a
           className="App-link"
